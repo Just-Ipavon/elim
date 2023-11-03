44b7e39a8eca12bef67a6ea1520053ca47520527
Just-Ipavon~ <pachiruo@gmail.com>
2023-11-03 17:14:42 +0100

    .

--- Algoritmi Esame/canny/canny.cpp
+++ Algoritmi Esame/canny/canny.cpp
@@ -1,112 +1,73 @@
 #include <opencv2/opencv.hpp>
 #include <stdlib.h>
 
-using namespace std;
 using namespace cv;
+using namespace std;
 
-void hysteresisThreshold(Mat &nms, Mat &dst, int lth, int hth)
-{
-    for (int i = 1; i < nms.rows - 1; i++)
-    {
-        for (int j = 1; j < nms.cols - 1; j++)
-        {
-            if (nms.at<uchar>(i, j) > hth)
-            {
-                dst.at<uchar>(i, j) = 255;
-                for (int k = -1; k <= 1; k++)
-                    for (int l = -1; l <= 1; l++)
-                        if (nms.at<uchar>(i + k, j + l) > lth && nms.at<uchar>(i + k, j + l) < hth)
-                            dst.at<uchar>(i + k, j + l) = 255;
+// imposto i valori del threshold e della dimensione del kernel
+
+const int th1 = 20;
+const int th2 = 50;
+const int ksize = 3;
+
+void Canny(const Mat src, Mat& dst){ // passo la matrice sorgente e la matrice destinazione
+    Mat gauss, dx, dy, magnitude, phase; // creo le matrici per la gaussiana, le derivate, la magnitudo e la fase
+    GaussianBlur(src, gauss, Size(5,5),0, 0);// applico la gaussiana
+    Sobel(gauss, dx, CV_32FC1, 1, 0, ksize); // applico la derivata in x CV_32FC1 è il tipo di dato 32 bit 1 canale float
+    Sobel(gauss, dy, CV_32FC1, 0, 1, ksize); // applico la derivata in y
+    cv::magnitude(dx,dy,magnitude); // calcolo la magnitudo
+    normalize(magnitude, magnitude, 0, 255, NORM_MINMAX, CV_8UC1); // normalizzo la magnitudo 8 bit unsigned 1 canale
+    cv::phase(dx, dy, phase, true); // calcolo la fase
+    uchar q, r; // creo due variabili di tipo unsigned char
+    for (int y = 1; y < magnitude.rows-1; y++){
+        for(int x=1; x<magnitude.cols-1; x++){
+            float angle = phase.at<float>(y,x) > 180 ? phase.at<float>(y,x) - 360: phase.at<float>(y,x); // calcolo l'angolo
+            uchar mag = magnitude.at<uchar>(y,x); // calcolo la magnitudo
+            if((angle< -157.5 || angle > 157.5) || (angle >= -22.5 && angle <= 22.5)){ // controllo se il gradiente è orizzontale
+                q = magnitude.at<uchar>(y,x-1);
+                r = magnitude.at<uchar>(y,x+1);
+            } else if((angle< -157.5 || angle > 112.5) || (angle >= -67.5 && angle <= 22.5)){ // controllo se il gradiente è diagonale 45
+                q = magnitude.at<uchar>(y+1,x-1);
+                r = magnitude.at<uchar>(y-1,x+1);
+            } else if((angle< -112.5 || angle > 67.5) || (angle >= -112.5 && angle <= 67.5)){ // controllo se il gradiente è verticale
+                q = magnitude.at<uchar>(y+1,x);
+                r = magnitude.at<uchar>(y-1,x);
+            } else if((angle< -67.5 || angle > 22.5) || (angle >= -157.5 && angle <= 112.5)){ // controllo se il gradiente è diagonale -45
+                q = magnitude.at<uchar>(y-1,x-1);
+                r = magnitude.at<uchar>(y+1,x+1);
             }
-            else
-            {
-                dst.at<uchar>(i, j) = 0;
+            if (mag < r || mag < q)
+                magnitude.at<uchar>(y,x) = 0;
+        }
+        for( int i=1; i<magnitude.rows-1;i++){ // applico il thresholding
+            for( int j = 1; j<magnitude.cols-1; j++){
+                uchar px = magnitude.at<uchar>(i,j);
+                if(px >= th2) px = 255; // se il pixel è maggiore del threshold alto lo imposto a 255
+                else if (px < th1) px = 0; // se il pixel è minore del threshold basso lo imposto a 0
+                else{
+                    bool sn = false; // se il pixel è compreso tra i due threshold controllo se è un pixel di bordo
+                    for (int x=-1; x<=1 && !sn; x++)
+					    for (int y=-1; y<=1 && !sn; y++)
+						    if (magnitude.at<uchar>(i+x,j+y) > th2)
+							    sn = true;
+                    if (sn) px = 255;
+                    else px = 0;
+                }
+                magnitude.at<uchar>(i,j) = px; // imposto il pixel
             }
         }
+    magnitude.copyTo(dst);
     }
+    
 }
 
-void nonMaximaSuppression(Mat &mag, Mat &orientations, Mat &nms)
-{
-    float angle;
-    for (int i = 1; i < mag.rows - 1; i++)
-    {
-        for (int j = 1; j < mag.cols - 1; j++)
-        {
-            angle = orientations.at<float>(i, j);
-            angle = angle > 180 ? angle - 360 : angle;
-            if ((-22.5 < angle && angle <= 22.5) || (157.5 < angle && angle <= -157.5))
-            { // horizontal
-                if (mag.at<uchar>(i, j) > mag.at<uchar>(i, j - 1) && mag.at<uchar>(i, j) > mag.at<uchar>(i, j + 1))
-                {
-                    nms.at<uchar>(i, j) = mag.at<uchar>(i, j);
-                }
-            }
-            else if ((-67.5 < angle && angle <= -22.5) || (112.5 < angle && angle <= 157.5))
-            { //+45
-                if (mag.at<uchar>(i, j) > mag.at<uchar>(i - 1, j + 1) && mag.at<uchar>(i, j) > mag.at<uchar>(i + 1, j - 1))
-                {
-                    nms.at<uchar>(i, j) = mag.at<uchar>(i, j);
-                }
-            }
-            else if ((-112.5 < angle && angle <= -67.5) || (67.5 < angle && angle <= 112.5))
-            { // vertical
-                if (mag.at<uchar>(i, j) > mag.at<uchar>(i - 1, j) && mag.at<uchar>(i, j) > mag.at<uchar>(i + 1, j))
-                {
-                    nms.at<uchar>(i, j) = mag.at<uchar>(i, j);
-                }
-            }
-            else if ((-157.5 < angle && angle <= -112.5) || (67.5 < angle && angle <= 22.5))
-            { //-45
-                if (mag.at<uchar>(i, j) > mag.at<uchar>(i - 1, j - 1) && mag.at<uchar>(i, j) > mag.at<uchar>(i + 1, j + 1))
-                {
-                    nms.at<uchar>(i, j) = mag.at<uchar>(i, j);
-                }
-            }
-        }
-    }
-}
-
-void canny(Mat &src, Mat &dst, int lth, int hth, int ksize)
-{
-
-    // 1
-    Mat gauss;
-    GaussianBlur(src, gauss, Size(3, 3), 0, 0);
-
-    // 2
-    Mat Dx, Dy, Dx2, Dy2, mag, orientations;
-    Sobel(gauss, Dx, CV_32FC1, 1, 0, ksize);
-    Sobel(gauss, Dy, CV_32FC1, 0, 1, ksize);
-    pow(Dx, 2, Dx2);
-    pow(Dy, 2, Dy2);
-    sqrt(Dx2 + Dy2, mag);
-    normalize(mag, mag, 0, 255, NORM_MINMAX, CV_8UC1);
-    phase(Dx, Dy, orientations, true);
-
-    // 3
-    Mat nms = Mat::zeros(src.rows, src.cols, CV_8UC1);
-    nonMaximaSuppression(mag, orientations, nms);
-
-    // 4
-    dst = Mat::zeros(src.rows, src.cols, CV_8UC1);
-    hysteresisThreshold(nms, dst, lth, hth);
-}
-
-int main(int argc, char **argv)
-{
-
-    Mat src = imread(argv[1], IMREAD_GRAYSCALE);
-    if (src.empty())
-        return -1;
-
-    Mat dst;
-    int lth = 20, hth = 80, ksize = 3;
-    canny(src, dst, lth, hth, ksize);
-
-    imshow("src", src);
-    imshow("dst", dst);
-    waitKey(0);
-
-    return 0;
+int main( int argc, char** argv ) {
+	Mat src = imread( argv[1], IMREAD_GRAYSCALE );
+	if(src.empty()) return -1;
+	Mat dst;
+	Canny(src,dst);
+	imshow("src", src);
+	imshow("dst", dst);
+	waitKey(0); 
+	return 0;
 }
